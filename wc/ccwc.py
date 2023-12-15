e95d1059055912630e872edce0472c97e6b45c63
unknown <adamkrieger87@gmail.com>
2023-12-15 10:40:25 -0500

    finished wc

--- wc/ccwc.py
+++ wc/ccwc.py
@@ -2,8 +2,8 @@ import sys
 
 
 class Wc:
-    def __init__(self, args):
-        self.args = args
+    def __init__(self, filename):
+        self.filename = filename
         self.chars = 0
         self.words = 0
         self.lines = 0
@@ -12,28 +12,16 @@ class Wc:
         self.read_file()
 
     def read_file(self):
-        with open(self.args, "r", encoding="utf-8") as file:
+        with open(self.filename, "r", encoding="utf-8") as file:
             for line in file:
                 self.lines += 1
                 self.words += len(line.split())
                 self.chars += len(line)
                 self.bytes += len(line.encode("utf-8"))
 
-            # f = file.read()
-
-            # for i, l in enumerate(f):
-            #     if l == "\n":
-            #         self.lines += 1
-
-            #     if l == " ":
-            #         self.words += 1
-
-            #     self.chars += 1
-            #     self.bytes += len(l.encode("utf-8"))
-
 
 def is_options_valid(options):
-    valid_options = ["c", "l", "w", "m"]
+    valid_options = ["-c", "-l", "-w", "-m"]
 
     for option in options:
         if option not in valid_options:
@@ -43,19 +31,26 @@ def is_options_valid(options):
 
 
 def main():
-    enum = {"c": "chars", "w": "words", "l": "lines", "m": "bytes"}
     if not is_options_valid(sys.argv[2:]):
-        print("arg does not exist")
+        print("argument does not exist")
         return
 
     x = Wc(sys.argv[1])
 
-    # ret = ""
-    # for arg in sys.argv[2:]:
-    #     ret += x.enum[arg] + " "
+    ret = ""
+    for arg in sys.argv[2:]:
+        if arg == "-c":
+            ret += str(x.chars)
+        if arg == "-w":
+            ret += str(x.words)
+        if arg == "-l":
+            ret += str(x.lines)
+        if arg == "-m":
+            ret += str(x.bytes)
 
-    # print(ret)
-    # print(x.lines, x.words, x.chars, x.bytes)
+        ret += " "
+
+    print(ret)
 
 
 if __name__ == "__main__":
